746e69b00c3f9da5d3d84b04f1ca2f5c6de67fa6
KIMJINHO <jhkim0131@naver.com>
2023-11-25 17:04:15 +0900

    login/register 추가

---
+++ app/src/main/java/com/example/mobile_programming_teamproject/UserAccount.kt
@@ -0,0 +1,9 @@
+package com.example.mobile_programming_teamproject
+
+data class UserAccount(
+    val idToken: String? = null,
+    val emailID: String? = null,
+    val password: String? = null,
+    val name: String? = null,
+    val birthdate: String? = null
+)
