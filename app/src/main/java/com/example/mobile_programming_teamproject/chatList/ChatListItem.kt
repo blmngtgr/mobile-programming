74dfbf7a33755ebbc3db89a73ed677a84a8d7102
hs-2191145-ParkYujin <118151172+blmngtgr@users.noreply.github.com>
2023-11-29 10:20:03 +0900

    Update ChatListItem.kt

--- app/src/main/java/com/example/mobile_programming_teamproject/chatList/ChatListItem.kt
+++ app/src/main/java/com/example/mobile_programming_teamproject/chatList/ChatListItem.kt
@@ -3,11 +3,9 @@ package com.example.mobile_programming_teamproject.chatList
 data class ChatListItem(
     val buyerId: String,
     val sellerId :String,
-    val buyerName: String,
-    val sellerName: String,
     val itemTitle: String,
     val key: Long
 ) {
 
-    constructor(): this("", "", "", "","",0)
+    constructor(): this("", "", "", 0)
 }
