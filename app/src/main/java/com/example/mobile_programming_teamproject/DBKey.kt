1742cfeee15c76c4e0384ee19b73a3c0e8acd7df
hs-1771304-kukseungbin <86177125+hs-1771304-kukseungbin@users.noreply.github.com>
2023-12-05 15:46:32 +0900

    Add files via upload

--- app/src/main/java/com/example/mobile_programming_teamproject/DBKey.kt
+++ app/src/main/java/com/example/mobile_programming_teamproject/DBKey.kt
@@ -2,7 +2,7 @@ package com.example.mobile_programming_teamproject
 
 class DBKey {
     companion object{
-        const val DB_ARTICLES = "Articles"
+        const val DB_ITEMS = "Items"
         const val DB_USERS = "Users"
         const val CHILD_CHAT = "Chat"
         const val DB_CHATS = "Chats"
